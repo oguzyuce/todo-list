e1e25f2c47812ab334cbca53af3a765c48cddcd0
Oguz <oguzcanyucetr@gmail.com>
2022-01-23 13:52:41 +0300

    Commit on todo-list

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input"> What's need to be done? </label>
+    </h2>
+    <input
+      type="text"
+      name="new-todo"
+      id="new-todo-input"
+      autocomplete="off"
+      v-model.trim.lazy="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      label: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("todo-added", this.label);
+      this.label = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
